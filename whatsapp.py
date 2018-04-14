faccfb7f0921e2fe11feb7fb2a5b2b19c73e5095
safwankdb <kdbeatbox@gmail.com>
2018-04-15 01:19:54 +0530

    Update whatsapp.py

--- whatsapp.py
+++ whatsapp.py
@@ -3,10 +3,9 @@ import time
 web = webdriver.Chrome()
 web.get('http://web.whatsapp.com')
 time.sleep(10)
-#print('Enter name of the victim')
-#name=input()
-elem = web.find_element_by_xpath("//span[@title=**********]")
-#replace ************* by the contact's name
+print('Enter name of the victim')
+name="'"+input()+"'"
+elem = web.find_element_by_xpath("//span[@title={name}]".format(name=name))
 elem.click()
 elem1 = web.find_element_by_xpath("//div[@contenteditable='true']")
 for i in range(1,1000):
@@ -14,4 +13,3 @@ for i in range(1,1000):
     elem1.send_keys('\n')
 elem1.send_keys('\nYou are being spammed by my python bot bitch')
 web.find_element_by_class_name("_2lkdt").click()
-
