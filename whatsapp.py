c3e3a569f7e549adbca3e5b9cb517a4582ab04ea
safwankdb <kdbeatbox@gmail.com>
2018-04-15 01:54:02 +0530

    Uses cookies to remember login.

--- whatsapp.py
+++ whatsapp.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import time
-web = webdriver.Chrome()
+opt=Options()
+opt.add_argument("user-data-dir=/tmp/")
+web = webdriver.Chrome(chrome_options=opt)
 web.get('http://web.whatsapp.com')
 time.sleep(10)
 print('Enter name of the victim')
@@ -10,6 +13,5 @@ elem.click()
 elem1 = web.find_element_by_xpath("//div[@contenteditable='true']")
 for i in range(1,1000):
     elem1.send_keys(i)
-    elem1.send_keys('\n')
 elem1.send_keys('\nYou are being spammed by my python bot bitch')
 web.find_element_by_class_name("_2lkdt").click()
