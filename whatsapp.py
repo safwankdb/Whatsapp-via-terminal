8d7ffad897ceab4965cfc2316fd5cdd484d807e1
safwankdb <kdbeatbox@gmail.com>
2018-04-15 00:17:47 +0530

    Add files via upload

---
+++ whatsapp.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+import time
+web = webdriver.Chrome()
+web.get('http://web.whatsapp.com')
+time.sleep(10)
+print('Enter name of the victim')
+name=input()
+elem = web.find_element_by_xpath("//span[@title=name]")
+elem.click()
+elem1 = web.find_element_by_xpath("//div[@contenteditable='true']")
+for i in range(1,1000):
+    elem1.send_keys(i)
+    elem1.send_keys('\n')
+elem1.send_keys('\nYou are being spammed by my python bot bitch')
+web.find_element_by_class_name("_2lkdt").click()
+
