f2156396c8de0c7cb9a0ef405489216b31d1f934
Mohd Safwan <kdbeatbox@gmail.com>
2018-04-15 19:14:59 +0530

    Added option to send an image/video, send custom message,search partial names

--- whatsapp.py
+++ whatsapp.py
@@ -1,17 +1,34 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
+import os
+import sys
 opt=Options()
-opt.add_argument("user-data-dir=/tmp/")
+opt.add_argument("user-data-dir=" + os.path.dirname(sys.argv[0]))
 web = webdriver.Chrome(chrome_options=opt)
+time.sleep(5)
 web.get('http://web.whatsapp.com')
-time.sleep(10)
 print('Enter name of the victim')
-name="'"+input()+"'"
-elem = web.find_element_by_xpath("//span[@title={name}]".format(name=name))
-elem.click()
-elem1 = web.find_element_by_xpath("//div[@contenteditable='true']")
-for i in range(1,1000):
-    elem1.send_keys(i)
-elem1.send_keys('\nYou are being spammed by my python bot bitch')
+name=input()
+searchBar=web.find_element_by_class_name("_2MSJr")
+searchBar.send_keys(name)
+time.sleep(1)
+elem1 = web.find_element_by_class_name("_1wjpf")
+elem1.click()
+print("Type your message below. Press Enter to send.")
+message=input()
+ElementTree = web.find_element_by_xpath("//div[@contenteditable='true']")
+elem.send_keys(message)
 web.find_element_by_class_name("_2lkdt").click()
+print("Enter y to attach an image, enter anything else oterwise")
+resp=input()
+if resp=='y':
+	attach = web.find_element_by_xpath("//div[@title='Attach']")
+	attach.click()
+	photoButton = web.find_elements_by_xpath('//*[@id="main"]/header/div[3]/div/div[2]/span/div/div/ul/li[1]/input')
+	print("Enter image path")
+	path=input()
+	photoButton[0].send_keys(path)
+	time.sleep(3)
+	sendImage=web.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/span/div/span/div/div/div[2]/span[2]/div/div/span');
+	sendImage.click()
